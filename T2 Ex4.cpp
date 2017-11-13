2e28c2a8bdd752f3ab88558834a17f967b7d9a66
Baron Tiberiu <baron_tibi2010@yahoo.com>
2017-11-13 18:54:30 +0200

    Update T2 Ex4.cpp

--- T2 Ex4.cpp
+++ T2 Ex4.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main(){
     int n,a,x,i=0,c=0;
-    int v[12]={31,27,31,30,31,30,31,31,30,31,30,31};
+    int v[12]={31,28,31,30,31,30,31,31,30,31,30,31};
     string l[12]={" ianuarie "," februarie "," martie "," aprilie"," mai "," iunie "," iulie "," august "," septembrie "," octombrie "," noiembrie "," decembrie "};
     cout<<"Introdu un an:";
     cin>>a;
     cout<<"Si un numar de zile:";
     cin>>x;
     if((a%400==0)||(a%4==0)&&(a%100!=0))
-        v[1]=28;
+        v[1]=29;
     if(x<=366){
             if(x<=v[i]){
                 n=x;
